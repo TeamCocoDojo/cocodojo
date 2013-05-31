c585b8f4317bc0d224397bab1a4e3b355aae2e24
seanxiaoxiao <seanxiaoxiao@hotmail.com>
2013-05-30 20:13:03 -0700

    Tree View

--- client/github.js
+++ client/github.js
@@ -1,32 +1,113 @@
 var cocodojo = cocodojo || {};
 
-cocodojo.github = cocodojo.github || {};
+cocodojo.githubAPI = cocodojo.githubAPI || {};
 
-cocodojo.github.getAccessToken = function() {
+cocodojo.githubAPI.getAccessToken = function() {
   if (Meteor.user()) {
     var user = Meteor.user();
     return user.services.github.accessToken;
   }
 };
 
-cocodojo.github.getUsername = function() {
+cocodojo.githubAPI.getUsername = function() {
   if (Meteor.user()) {
     var user = Meteor.user();
     return user.services.github.username;
   }
 };
 
-cocodojo.github.getRepos = function() {
-  Meteor.http.get("https://api.github.com/user/repos?access_token=" + cocodojo.github.getAccessToken(),
+cocodojo.githubAPI.getRepos = function(callback) {
+  var accessToken = cocodojo.githubAPI.getAccessToken();
+  console.log(accessToken);
+  Meteor.http.get("https://api.github.com/user/repos?access_token=" + accessToken,
     function (error, result) {
       if (result.statusCode === 200) {
-        console.log(result);
+        callback(result);
       }
-    });
+    }
+  );
+};
+
+cocodojo.githubAPI.getFiles = function() {
+
+};
+
+cocodojo.githubRepo = function(repo) {
+  return this;
+};
+
+cocodojo.githubRepo.prototype = {
+
+  getFiles: function() {
+
+  },
+
+  getBranches: function() {
+
+  }
+};
+
+cocodojo.githubFile = function(file) {
+  return this;
+};
+
+cocodojo.githubFile.prototype = {
+
+  getContent: function() {
+
+  },
+
+  getType: function() {
+
+  },
+
+  getChildren: function() {
+
+  },
+
+  updateFile: function(content) {
+
+  },
+
+  deleteFile: function() {
+
+  }
+
+};
+
+
+Template.contents.userName = function() {
+  return cocodojo.githubAPI.getUsername();
 };
 
 Template.contents.events({
   'click input' : function () {
-    cocodojo.github.getRepos();
+    cocodojo.githubAPI.getRepos();
   }
 });
+
+Template.contents.rendered = function() {
+
+  cocodojo.githubAPI.getRepos(function(repos) {
+    var allRepo = [];
+    for (var i = 0; i < repos.data.length; i++) {
+        var repo = {
+          name: repos.data[i].name,
+          type: "folder",
+        };
+        allRepo.push(repo);
+    }
+    var dataSourceTree = new TreeDataSource({
+      data: allRepo,
+      delay: 400
+    });
+
+    $('#ex-tree').tree({
+      dataSource: dataSourceTree,
+      loadingHTML: '<div class="static-loader">Loading...</div>',
+      multiSelect: true,
+      cacheItems: true
+    });
+  });
+};
+  
