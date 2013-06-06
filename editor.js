949caed1ddde9208aac2f42d47162eab9b58a5cf
Lydian Lee <lydian@cmu.edu>
2013-06-05 21:05:34 -0700

    fix indent=2

--- editor.js
+++ editor.js
@@ -9,7 +9,7 @@ if(Meteor.isServer){
   function onReceiveOperation (json) {
     var operation = ot.Operation.fromJSON(JSON.parse(json));
   }
-  
+
 }
 if(Meteor.isClient){
 
@@ -18,18 +18,18 @@ if(Meteor.isClient){
 
   var CocoDojoRouter = Backbone.Router.extend({
     routes: {
-     ":session_id": "dojo",
-     ":session_id/sync": "sync"
-   },
-   dojo: function(codeSessionId) {
-    Session.set("codeSessionId", codeSessionId);
-    cocodojo.client = new ot.Client(0);
+      ":session_id": "dojo",
+      ":session_id/sync": "sync"
+    },
+    dojo: function(codeSessionId) {
+      Session.set("codeSessionId", codeSessionId);
+      cocodojo.client = new ot.Client(0);
 
-    cocodojo.client.applyOperation = function (operation) {
+      cocodojo.client.applyOperation = function (operation) {
 
-    };
+      };
 
-    cocodojo.client.sendOperation = function (operation) {
+      cocodojo.client.sendOperation = function (operation) {
         // send the operation to the server, e.g. with ajax:
         console.log(operation);
         $.ajax({
@@ -52,25 +52,25 @@ if(Meteor.isClient){
       }
 
     },
-  sync :function(codeSessionId) {
+    sync :function(codeSessionId) {
 
-  }
-    });
-Router = new CocoDojoRouter;
-
-Meteor.startup(function () {
-  Backbone.history.start({pushState: true});
-  $(document).ready(function() {
-    if (window.location.pathname == "/") {
-      var codeSessionId = CodeSession.insert({name: "New Dojo"});
-      Router.navigate(codeSessionId, false);
     }
-  }); 
-});
+  });
+  Router = new CocoDojoRouter;
 
-Template.editor.rendered = function(){
-  var codeSession = CodeSession.find();
-  var codeSessionId = Session.get("codeSessionId");
+  Meteor.startup(function () {
+    Backbone.history.start({pushState: true});
+    $(document).ready(function() {
+      if (window.location.pathname == "/") {
+        var codeSessionId = CodeSession.insert({name: "New Dojo"});
+        Router.navigate(codeSessionId, false);
+      }
+    }); 
+  });
+
+  Template.editor.rendered = function(){
+    var codeSession = CodeSession.find();
+    var codeSessionId = Session.get("codeSessionId");
 
   /* create cocodojo object*/
   editor = {};
@@ -82,20 +82,20 @@ Template.editor.rendered = function(){
   editor.editorInstance.setTheme("ace/theme/monokai");
   editor.editorInstance.getSession().setMode("ace/mode/javascript");
   editor.editorInstance.getSession().getDocument().on("change", function(e){
-    
+
     cocodojo.client.sendOperation(e.data);
   });
 
-      // Fix me: typing simutaneously is not working very good. It should be something related to filter deltas. 
-      editor.update = function(deltas){
-       if(deltas === undefined){ return false; }
-       var pendDeltas = [];
-       for(var i=editor.currentDelta; i<deltas.length; ++i){
-        if(deltas[i].sender_uid != editor.local_uid){
-         pendDeltas.push(deltas[i].delta);
-       }
-     }
-     if(pendDeltas.length > 0){
+  // Fix me: typing simutaneously is not working very good. It should be something related to filter deltas. 
+  editor.update = function(deltas){
+    if(deltas === undefined){ return false; }
+    var pendDeltas = [];
+    for(var i=editor.currentDelta; i<deltas.length; ++i){
+      if(deltas[i].sender_uid != editor.local_uid){
+        pendDeltas.push(deltas[i].delta);
+      }
+    }
+    if(pendDeltas.length > 0){
       editor.updateDue = true;
       editor.editorInstance.getSession().getDocument().applyDeltas(pendDeltas);
     }
@@ -104,15 +104,15 @@ Template.editor.rendered = function(){
   };
 
   /*** Initialization ***/
-      // Fix me: Directly do "CodeSession.findOne" always return undefined. Maybe we should find a event to bind
-      // Fix me: After several reloads, the CodeSession.findOne will always return undefined. Not yet figure out the cause. 
-      editor.init = function(){
-       var deltas = CodeSession.findOne({_id: Session.get("codeSessionId")});
-       if( typeof deltas != "undefined"){
-            //apply latest changes
-            editor.update(deltas.Deltas);
+  // Fix me: Directly do "CodeSession.findOne" always return undefined. Maybe we should find a event to bind
+  // Fix me: After several reloads, the CodeSession.findOne will always return undefined. Not yet figure out the cause. 
+  editor.init = function(){
+    var deltas = CodeSession.findOne({_id: Session.get("codeSessionId")});
+    if( typeof deltas != "undefined"){
+      //apply latest changes
+      editor.update(deltas.Deltas);
 
-            // add online users
+      // add online users
 /*
             CodeSession.update(
                {_id: Session.get("codeSessionId")},
@@ -121,40 +121,40 @@ Template.editor.rendered = function(){
                }
                );*/
 
-}
-else{
-  console.log("unable to connect to the server, retry in 1 second");
+    }
+    else{
+      console.log("unable to connect to the server, retry in 1 second");
+      setTimeout("editor.init()", 1000);
+    }
+
+  };
   setTimeout("editor.init()", 1000);
+
+  var mongoQuery = CodeSession.find({_id: Session.get("codeSessionId")});
+  mongoQuery.observe({
+    changed : function(newDoc, oldIndex, oldDoc) {
+      console.log("observed");   
+      editor.update(newDoc.Deltas);
+      //editor.addComment(newDoc.Comments);
+      console.log(newDoc.OnlineUsers);
+    }
+  });
+
+  // Fix me: not working, should do server side validation
+  // before leaves, update online user lists
+  $(window).unload( function() {
+    CodeSession.update(
+      {_id: Session.get("codeSessionId")},
+      { $pull:
+        { OnlineUsers: {name: editor.local_uid} }
+
+      }
+    );
+  });
+  console.log(ot);
+  var client = new ot.Client(0);
+  };
+
+
+
 }
-
-};
-setTimeout("editor.init()", 1000);
-
-var mongoQuery = CodeSession.find({_id: Session.get("codeSessionId")});
-mongoQuery.observe({
- changed : function(newDoc, oldIndex, oldDoc) {
-  console.log("observed");   
-  editor.update(newDoc.Deltas);
-            //editor.addComment(newDoc.Comments);
-            console.log(newDoc.OnlineUsers);
-          }
-        });
-
-      // Fix me: not working, should do server side validation
-      // before leaves, update online user lists
-      $(window).unload( function() {
-       CodeSession.update(
-        {_id: Session.get("codeSessionId")},
-        { $pull:
-         { OnlineUsers: {name: editor.local_uid} }
-
-       }
-       );
-     });
-      console.log(ot);
-      var client = new ot.Client(0);
-    };
-
-
-
-  }
