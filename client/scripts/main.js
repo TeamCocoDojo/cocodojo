6cae05356d705f1268681350da40fa3405d8a644
clydeli <clyde1008li@gmail.com>
2013-06-08 02:51:51 -0700

    unpretty solution for aligning componenets with the edge

---
+++ client/scripts/main.js
@@ -0,0 +1,8 @@
+Meteor.startup(function () {
+  $(document).ready(function() {
+    $('#content').css('height', $('body').height()-62+'px');
+    $(window).resize(function() { 
+      $('#content').css('height', $('body').height()-62+'px');
+    });
+  });
+});
