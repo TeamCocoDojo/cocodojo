8e0a038f64141416dddcea07183755913261ac01
Lydian Lee <lydian@cmu.edu>
2013-06-17 16:08:58 -0700

    update editor content based on github repo selected file

--- client/scripts/repoview.js
+++ client/scripts/repoview.js
@@ -1,6 +1,9 @@
 var DataSource = function (owner, repoName) {
   this.repo = cocodojo.githubObj.getRepo(owner, repoName);
 }
+DataSource.prototype.getContent = function (sha, callback) {
+  this.repo.getBlob(sha, callback);
+};
 DataSource.prototype.data =  function (options, callback) {
   var self = this;
   var sha = options.sha || "master";
@@ -16,11 +19,20 @@ DataSource.prototype.data =  function (options, callback) {
   });
 }
 
-Template.repoview.rendered = function() {
-  $(document).on("repoSelected", function(e, repoInfo) {
-    //Put your logic here
-    var dataSource = new DataSource(repoInfo.owner, repoInfo.name);
-    $('#repoTree').modal('hide');
-    $('#ex-tree').tree({ dataSource: dataSource });
+$(document).on("repoSelected", function(e, repoInfo) {
+  console.log("repoSelected");
+  //Put your logic here
+  var dataSource = new DataSource(repoInfo.owner, repoInfo.name);
+  $('#repoTree').modal('hide');
+  $('#ex-tree').tree({ dataSource: dataSource }).on("selected", function(event, selectedObjs){
+    var selectedItem = selectedObjs.info[0];
+    console.log("item selected");
+    dataSource.getContent(selectedItem.sha, function(err, data){
+      $(document).trigger("repoFileSelected", {name: selectedItem.name, sha:selectedItem.sha, content: data});
+    });
   });
+});
+
+Template.repoview.rendered = function() {
+
 };
