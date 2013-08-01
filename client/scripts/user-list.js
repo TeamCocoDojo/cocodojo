76868bd5e8e07b6eb4bae949a15c5df9909df56c
clydeli <clyde1008li@gmail.com>
2013-07-31 21:07:37 -0700

    new user list layout

--- client/scripts/user-list.js
+++ client/scripts/user-list.js
@@ -1,3 +1,25 @@
 Template.userList.users = function(){
-  return SessionUsers.find({});
+  var
+    userList = [];
+    sessionUsers = SessionUsers.find({});
+
+  sessionUsers.forEach(function(user){
+    userList.push({
+      _id: user._id,
+      userId: user.userId,
+      userColor: user.userColor,
+      initial: user.username.slice(0, 1).toUpperCase(),
+      username: user.username
+    });
+    console.log(user);
+  });
+
+  return userList;
+};
+
+Template.userList.rendered = function(){
+  $('.user-instance').on('mouseenter', function(){
+    //console.log("test");
+    $(this).tooltip('show');
+  });
 };
