55fcc3dd928744b4e613687fb80e02d136dc8da6
Lydian Lee <lydian@cmu.edu>
2013-08-02 12:55:18 -0700

    fix indent

--- client/scripts/file-list.js
+++ client/scripts/file-list.js
@@ -1,267 +1,267 @@
 var FolderList = function (owner, repoName, branch, element) {
-	this.branch = branch;
-	this.owner = owner;
-	this.repoName = repoName;
-	this.element = element;
-	this.folders = {};
-	element.empty();
+  this.branch = branch;
+  this.owner = owner;
+  this.repoName = repoName;
+  this.element = element;
+  this.folders = {};
+  element.empty();
 }
 FolderList.prototype.getRepo = function(){
-	var githubObj = cocodojo.getGithubObj();
-	return githubObj.getRepo(this.owner, this.repoName);
+  var githubObj = cocodojo.getGithubObj();
+  return githubObj.getRepo(this.owner, this.repoName);
 }
 FolderList.prototype.getContent = function (path, callback) {
-	Meteor.call("getGithubContent", Session.get("codeSessionId"), path, function(err, result) {
-		console.log(result);
-		callback(err, result);
-	});
+  Meteor.call("getGithubContent", Session.get("codeSessionId"), path, function(err, result) {
+    console.log(result);
+    callback(err, result);
+  });
 };
 
 FolderList.prototype.addToFolderList = function ( data ) {
-	var pathes = data.path.split("/");
-	var targetFolder = this.element;
-	for(var i=0; i< pathes.length-1; i++){
-		targetFolder = $(_.filter(targetFolder.children(".tree-folder"), function(item) {
-			return $($($(item).children(".tree-folder-header")[0]).children(".tree-folder-name")[0]).text() == pathes[i];
-		})[0]).children(".tree-folder-content");
-	}
-	if(data.type == "file") 
-		var includeItem = cocodojo.folderlist.createFileItem(data);
-	else
-		var includeItem = cocodojo.folderlist.createFolderItem(data)
+  var pathes = data.path.split("/");
+  var targetFolder = this.element;
+  for(var i=0; i< pathes.length-1; i++){
+    targetFolder = $(_.filter(targetFolder.children(".tree-folder"), function(item) {
+      return $($($(item).children(".tree-folder-header")[0]).children(".tree-folder-name")[0]).text() == pathes[i];
+    })[0]).children(".tree-folder-content");
+  }
+  if(data.type == "file") 
+    var includeItem = cocodojo.folderlist.createFileItem(data);
+  else
+    var includeItem = cocodojo.folderlist.createFolderItem(data)
 
-	if(targetFolder.children(":contains('" + data.name + "')").length >0 && 
-		 $(targetFolder.children(":contains('" + data.name + "')")[0]).text() == data.name ) return; 
+  if(targetFolder.children(":contains('" + data.name + "')").length >0 && 
+     $(targetFolder.children(":contains('" + data.name + "')")[0]).text() == data.name ) return; 
 
-		targetFolder.append(includeItem);
-		$.contextMenu({
-			selector: '.tree-item',
-			items: {
-				"rename": {
-					name: "Rename File",
-					callback: function() {
-						var fileName = window.prompt("Please input the name of the file");
-						if (fileName) {
-							var fullPath = $(this).data("path") + "/" +fileName;
-							$(document).trigger("renameFile", fullPath);
-						}
-					}
-				},
-				"delete": {
-					name: "Delete File",
-					callback: function() {
-						$(document).trigger("deleteFile", fullPath);
-					}
-				}
-			}
-		});
-		$.contextMenu({
-			selector: '.tree-folder-header',
-			items: {
-				"add": {
-					name: "Add File",
-					callback: function(key, obj) {
-						var item = $(obj.$trigger[0]);
-						var fileName = window.prompt("Please input the name of the file");
-						if (fileName) {
-							var fullPath = $(this).data("path") + "/" +fileName;
-							$(document).trigger("addFile", {path: fullPath, name: fileName});
-						}
-					}
-				},
-				"rename": {
-					name: "Rename Directory",
-					callback: function() {
-						var fileName = window.prompt("Please input the name of the file");
-						if (fileName) {
-							var fullPath = $(this).data("path") + fileName
-							$(document).trigger("renameFile", fullPath);
-						}
-					}
-				},
-				"delete": {
-					name: "Delete Directory",
-					callback: function() {
-						$(document).trigger("deleteFile", fullPath);
-					}
-				}
-			}
-		});
+    targetFolder.append(includeItem);
+    $.contextMenu({
+      selector: '.tree-item',
+      items: {
+        "rename": {
+          name: "Rename File",
+          callback: function() {
+            var fileName = window.prompt("Please input the name of the file");
+            if (fileName) {
+              var fullPath = $(this).data("path") + "/" +fileName;
+              $(document).trigger("renameFile", fullPath);
+            }
+          }
+        },
+        "delete": {
+          name: "Delete File",
+          callback: function() {
+            $(document).trigger("deleteFile", fullPath);
+          }
+        }
+      }
+    });
+    $.contextMenu({
+      selector: '.tree-folder-header',
+      items: {
+        "add": {
+          name: "Add File",
+          callback: function(key, obj) {
+            var item = $(obj.$trigger[0]);
+            var fileName = window.prompt("Please input the name of the file");
+            if (fileName) {
+              var fullPath = $(this).data("path") + "/" +fileName;
+              $(document).trigger("addFile", {path: fullPath, name: fileName});
+            }
+          }
+        },
+        "rename": {
+          name: "Rename Directory",
+          callback: function() {
+            var fileName = window.prompt("Please input the name of the file");
+            if (fileName) {
+              var fullPath = $(this).data("path") + fileName
+              $(document).trigger("renameFile", fullPath);
+            }
+          }
+        },
+        "delete": {
+          name: "Delete Directory",
+          callback: function() {
+            $(document).trigger("deleteFile", fullPath);
+          }
+        }
+      }
+    });
 }
 
 FolderList.prototype.createFolderItem = function ( data ) {
-	var me = this;
-	var header = $("<div/>").addClass('tree-folder-header').append("<i class='icon-folder-close'></i>").append("<div class='tree-folder-name'>" + data.name + "</div>");
-	var content = $("<div/>").addClass('tree-folder-content').hide();
-	var element = $("<div/>").addClass('tree-folder');
-	header.attr("data-path", data.path);
-	header.data(data);
-	header.appendTo(element);
-	content.appendTo(element);
-	header.click(function(evt){
-		var element = $($(this).parents(".tree-folder")[0]);
-		$(element.find(".tree-folder-content")[0]).toggle();
-		var selectedItem = element.data();
-		me.getFolderList({sha: selectedItem.sha, path: selectedItem.path, element: $(element.find(".tree-folder-content")[0])});
-	});
+  var me = this;
+  var header = $("<div/>").addClass('tree-folder-header').append("<i class='icon-folder-close'></i>").append("<div class='tree-folder-name'>" + data.name + "</div>");
+  var content = $("<div/>").addClass('tree-folder-content').hide();
+  var element = $("<div/>").addClass('tree-folder');
+  header.attr("data-path", data.path);
+  header.data(data);
+  header.appendTo(element);
+  content.appendTo(element);
+  header.click(function(evt){
+    var element = $($(this).parents(".tree-folder")[0]);
+    $(element.find(".tree-folder-content")[0]).toggle();
+    var selectedItem = element.data();
+    me.getFolderList({sha: selectedItem.sha, path: selectedItem.path, element: $(element.find(".tree-folder-content")[0])});
+  });
 
-	header.mousedown(function(e) {
-		if (e.button == 2) {
-			e.stopPropagation();
-			$("#folder-menu").css("top", $(this).css("top"));
-			$("#folder-menu").css("left", $(this).css("left"));
-		}
-	});
-	return element;
+  header.mousedown(function(e) {
+    if (e.button == 2) {
+      e.stopPropagation();
+      $("#folder-menu").css("top", $(this).css("top"));
+      $("#folder-menu").css("left", $(this).css("left"));
+    }
+  });
+  return element;
 };
 
 FolderList.prototype.createFileItem = function (data) {
-	var me = this;
-	var element = $("<div></div>").addClass('tree-item').append("<i class='tree-dot'></i>").append("<div class='tree-item-name'>" + data.name + "</div>");
-	element.data(data);
-	element.click(function(evt){
-		var selectedItem = $(this).data();
-		me.getContent(selectedItem.path, function(err, data){
-			$(document).trigger("repoFileSelected", {owner: me.owner, repo: me.repoName ,name: selectedItem.name, sha:selectedItem.sha, content: data.content, path:selectedItem.path});
-		});
-	});
-	return element;
+  var me = this;
+  var element = $("<div></div>").addClass('tree-item').append("<i class='tree-dot'></i>").append("<div class='tree-item-name'>" + data.name + "</div>");
+  element.data(data);
+  element.click(function(evt){
+    var selectedItem = $(this).data();
+    me.getContent(selectedItem.path, function(err, data){
+      $(document).trigger("repoFileSelected", {owner: me.owner, repo: me.repoName ,name: selectedItem.name, sha:selectedItem.sha, content: data.content, path:selectedItem.path});
+    });
+  });
+  return element;
 };
 
 FolderList.prototype.getFolderList = function (options, callback){
-	if(options.path !== undefined ) {
-		var folders = options.path.split("/");
-		var folder = this.folders;
-		for(var i=0; i< folders.length; i++){
-			if (folder[folders[i]] === undefined) break;
-			folder = folder[folders[i]];
-			if(i==folders.length -1 ) return folder;
-		}
-	}
-	var pathes = (options.path !== undefined )?options.path.split("/"):[];
-	var currentFolder = this.folders;
-	for(var i=0; i< pathes.length; i++) {
-		if(currentFolder[pathes[i]] === undefined) currentFolder[pathes[i]] = {};
-		currentFolder = currentFolder[pathes[i]];
-	}
+  if(options.path !== undefined ) {
+    var folders = options.path.split("/");
+    var folder = this.folders;
+    for(var i=0; i< folders.length; i++){
+      if (folder[folders[i]] === undefined) break;
+      folder = folder[folders[i]];
+      if(i==folders.length -1 ) return folder;
+    }
+  }
+  var pathes = (options.path !== undefined )?options.path.split("/"):[];
+  var currentFolder = this.folders;
+  for(var i=0; i< pathes.length; i++) {
+    if(currentFolder[pathes[i]] === undefined) currentFolder[pathes[i]] = {};
+    currentFolder = currentFolder[pathes[i]];
+  }
 }
 
 Template.repoview.events({
-	'click #login': function (evt) {
-		Meteor.loginWithGithub({
-			requestPermissions: ['user', 'public_repo']
-		},function (err) {
-			if (err){
-				alert(err);
-				console.log(err);
-			}
-		});
-		evt.preventDefault();
-	},
-	'click #logout': function (evt) {
-		Meteor.logout(function (err) {
-			if (err)
-				Meteor._debug(err);
-		});
-		evt.preventDefault();
-	}
+  'click #login': function (evt) {
+    Meteor.loginWithGithub({
+      requestPermissions: ['user', 'public_repo']
+    },function (err) {
+      if (err){
+        alert(err);
+        console.log(err);
+      }
+    });
+    evt.preventDefault();
+  },
+  'click #logout': function (evt) {
+    Meteor.logout(function (err) {
+      if (err)
+        Meteor._debug(err);
+    });
+    evt.preventDefault();
+  }
 });
 
 $(document).on("addFile", function(evt, data) {
-	console.log("on add file:", data);
-	var fileName = data.name;
-	var filePath = data.path;
-	FileFolder.insert({
-		codeSessionId: Session.get("codeSessionId"),
-		type: "file",
-		path: data.path, 
-		name: data.name
-	});
-	$(document).trigger("doneAddFile", {owner: cocodojo.repoOwner, repo: cocodojo.repoName, name: fileName, content: "", path: filePath});
+  console.log("on add file:", data);
+  var fileName = data.name;
+  var filePath = data.path;
+  FileFolder.insert({
+    codeSessionId: Session.get("codeSessionId"),
+    type: "file",
+    path: data.path, 
+    name: data.name
+  });
+  $(document).trigger("doneAddFile", {owner: cocodojo.repoOwner, repo: cocodojo.repoName, name: fileName, content: "", path: filePath});
 });
 
 $(document).on("addFolder", function(evt, data) {
-	var fileName = data.name;
-	var filePath = data.path;
-	FileFolder.insert({
-		codeSessionId: Session.get("codeSessionId"),
-		type: "folder",
-		path: data.path, 
-		name: data.name,
-		status: "new"
-	});
-	$(document).trigger("doneAddFolder", {owner: cocodojo.repoOwner, repo: cocodojo.repoName, name: fileName, content: "", path: filePath});
+  var fileName = data.name;
+  var filePath = data.path;
+  FileFolder.insert({
+    codeSessionId: Session.get("codeSessionId"),
+    type: "folder",
+    path: data.path, 
+    name: data.name,
+    status: "new"
+  });
+  $(document).trigger("doneAddFolder", {owner: cocodojo.repoOwner, repo: cocodojo.repoName, name: fileName, content: "", path: filePath});
 });
 
 function setGithubFileTree(fields){
-	if(fields.githubRepo === undefined) return;
-	var githubRepo = fields.githubRepo;
-	var githubHost = fields.githubHost || cocodojo.codeSessionHost || "";
-	var githubBranch = fields.githubBranch || cocodojo.githubBranch || "master";
-	if( cocodojo.githubRepo=== undefined || cocodojo.githubRepo != githubRepo ){
-		cocodojo.githubRepo = githubRepo;
-		$(document).trigger("repoSelected", {owner: githubHost, name: githubRepo, branch: githubBranch  });
-		if(cocodojo.folderlist === undefined) 
-			cocodojo.folderlist = new FolderList( githubHost, githubRepo, githubBranch, $("#ex-tree"));
-		$('#repoTree').modal('hide');
-	}
-	cocodojo.codeSessionHost = fields.codeSessionHost;
+  if(fields.githubRepo === undefined) return;
+  var githubRepo = fields.githubRepo;
+  var githubHost = fields.githubHost || cocodojo.codeSessionHost || "";
+  var githubBranch = fields.githubBranch || cocodojo.githubBranch || "master";
+  if( cocodojo.githubRepo=== undefined || cocodojo.githubRepo != githubRepo ){
+    cocodojo.githubRepo = githubRepo;
+    $(document).trigger("repoSelected", {owner: githubHost, name: githubRepo, branch: githubBranch  });
+    if(cocodojo.folderlist === undefined) 
+      cocodojo.folderlist = new FolderList( githubHost, githubRepo, githubBranch, $("#ex-tree"));
+    $('#repoTree').modal('hide');
+  }
+  cocodojo.codeSessionHost = fields.codeSessionHost;
 };
 
 $(document).on("repoSelectedByHost", function(evt, data) {
-	$("#ex-tree").addClass("hide");
-	$(".loading").removeClass("hide");
-	if(cocodojo.folderlist === undefined) 
-		cocodojo.folderlist = new FolderList( data.owner, data.name, data.branch, $("#ex-tree"));
-	Meteor.call("initGithubFolderList", Session.get("codeSessionId"), function(){
-		$("#ex-tree").removeClass("hide");
-		$(".loading").addClass("hide");
-	});
+  $("#ex-tree").addClass("hide");
+  $(".loading").removeClass("hide");
+  if(cocodojo.folderlist === undefined) 
+    cocodojo.folderlist = new FolderList( data.owner, data.name, data.branch, $("#ex-tree"));
+  Meteor.call("initGithubFolderList", Session.get("codeSessionId"), function(){
+    $("#ex-tree").removeClass("hide");
+    $(".loading").addClass("hide");
+  });
 });
 
 Template.repoview.rendered = function() {
-	CodeSession.find({_id: Session.get("codeSessionId")}).observeChanges({
-		added: function(id, fields){
-			setGithubFileTree(fields);
-			console.log(fields);
-			if (fields.githubRepo) {
-				$(".repo-name-branch").text("Repo Name: " + fields.githubRepo + " @ "+ fields.githubBranch);
-				$(".repo-host").text("Hosted by: " + fields.githubHost);
-			}
-		},
-		changed:function (id, fields){
-			setGithubFileTree(fields);
-			$(".repo-name-branch").text("Repo Name: " + fields.githubRepo + " @ "+ fields.githubBranch);
-			$(".repo-host").text("Hosted by: " + fields.githubHost);
-		}
-	});
-	FileFolder.find({codeSessionId: Session.get("codeSessionId")}).observeChanges({
-		added: function(id, itemObj) {
-			cocodojo.folderlist.addToFolderList(itemObj);
-		}
-	});
+  CodeSession.find({_id: Session.get("codeSessionId")}).observeChanges({
+    added: function(id, fields){
+      setGithubFileTree(fields);
+      console.log(fields);
+      if (fields.githubRepo) {
+        $(".repo-name-branch").text("Repo Name: " + fields.githubRepo + " @ "+ fields.githubBranch);
+        $(".repo-host").text("Hosted by: " + fields.githubHost);
+      }
+    },
+    changed:function (id, fields){
+      setGithubFileTree(fields);
+      $(".repo-name-branch").text("Repo Name: " + fields.githubRepo + " @ "+ fields.githubBranch);
+      $(".repo-host").text("Hosted by: " + fields.githubHost);
+    }
+  });
+  FileFolder.find({codeSessionId: Session.get("codeSessionId")}).observeChanges({
+    added: function(id, itemObj) {
+      cocodojo.folderlist.addToFolderList(itemObj);
+    }
+  });
 
-	$.contextMenu({
-		selector: '.tree',
-		items: {
-			"add": {
-				name: "Add File",
-				callback: function() {
-					var fileName = window.prompt("Please input the name of the file");
-					if (fileName) {
-						$(document).trigger("addFile", {path: fileName, name: fileName});
-					}
-				}
-			},
-			"add-folder": {
-				name: "Add Folder", 
-				callback: function() {
-					var folderName = window.prompt("Please input the name of the folder");
-					if (folderName) {
-						$(document).trigger("addFolder", {path: folderName, name: folderName});
-					}
-				}
-			}
-		}
-	});
+  $.contextMenu({
+    selector: '.tree',
+    items: {
+      "add": {
+        name: "Add File",
+        callback: function() {
+          var fileName = window.prompt("Please input the name of the file");
+          if (fileName) {
+            $(document).trigger("addFile", {path: fileName, name: fileName});
+          }
+        }
+      },
+      "add-folder": {
+        name: "Add Folder", 
+        callback: function() {
+          var folderName = window.prompt("Please input the name of the folder");
+          if (folderName) {
+            $(document).trigger("addFolder", {path: folderName, name: folderName});
+          }
+        }
+      }
+    }
+  });
 };
--- client/scripts/github-commit.js
+++ client/scripts/github-commit.js
@@ -3,44 +3,44 @@ var docs = null;
 var currentBranch = null;
 var targetBranch = null;
 function commitFail(err){
-	$("commitFailReason").text(err);
-	$('#commitBox').modal('hide');
-	$('#commitFail').modal('show');
+  $("commitFailReason").text(err);
+  $('#commitBox').modal('hide');
+  $('#commitFail').modal('show');
 }
 
 
 Template.commitBox.rendered = function() {
-	$('#commitBox').on('show', function () {
-		Meteor.call("getGithubContent", Session.get("codeSessionId"), "", function(result) {
-			console.log(arguments);
-		});
+  $('#commitBox').on('show', function () {
+    Meteor.call("getGithubContent", Session.get("codeSessionId"), "", function(result) {
+      console.log(arguments);
+    });
 
-		$(".loading").removeClass("hide");
-		$(document).on("doneCommit", function() {
-			docs = $.map(FileTab.find({codeSessionId: Session.get("codeSessionId")}).fetch(), function(file){
-				if(file.file.path === undefined) return null;
-				return {path: file.file.path, content: file.file.content };
-			});
-			$(".loading").addClass("hide");
-			$("#files-commited").empty();
-			docs.forEach(function(doc){
-				$("<li/>").text(doc.path).appendTo("#files-commited");
-			});
-			docs.forEach(function(doc){
-				$("<li/>").text(doc.path).appendTo("#files-commited");
-			});
-			$("#commitConfirm").removeAttr("disabled");
-		});
-	});
+    $(".loading").removeClass("hide");
+    $(document).on("doneCommit", function() {
+      docs = $.map(FileTab.find({codeSessionId: Session.get("codeSessionId")}).fetch(), function(file){
+        if(file.file.path === undefined) return null;
+        return {path: file.file.path, content: file.file.content };
+      });
+      $(".loading").addClass("hide");
+      $("#files-commited").empty();
+      docs.forEach(function(doc){
+        $("<li/>").text(doc.path).appendTo("#files-commited");
+      });
+      docs.forEach(function(doc){
+        $("<li/>").text(doc.path).appendTo("#files-commited");
+      });
+      $("#commitConfirm").removeAttr("disabled");
+    });
+  });
 }
 
 $(document).on("repoSelected", function(e, repoInfo){
-	$(document).trigger("updateGithubInfo", repoInfo);
-	cocodojo.repoOwner = repoInfo.owner;
-	cocodojo.repoName = repoInfo.name;
-	currentBranch = repoInfo.branch;
+  $(document).trigger("updateGithubInfo", repoInfo);
+  cocodojo.repoOwner = repoInfo.owner;
+  cocodojo.repoName = repoInfo.name;
+  currentBranch = repoInfo.branch;
 
-	var sessionSocket = io.connect(document.location.hostname + "/sesssion" + Session.get("codeSessionId"), {port: 3333});
+  var sessionSocket = io.connect(document.location.hostname + "/sesssion" + Session.get("codeSessionId"), {port: 3333});
     $(document).on("commitToGit", function() {
       console.log("Commit to git o");
       sessionSocket.emit("commit");
@@ -57,77 +57,77 @@ $(document).on("repoSelected", function(e, repoInfo){
       $(document).trigger("doneCommit");
     });
 
-	$("#branch-name").text(repoInfo.branch);
+  $("#branch-name").text(repoInfo.branch);
 
-	$("#btnCommitBox").click(function() {
-		$(document).trigger("commitToGit");
-	});
+  $("#btnCommitBox").click(function() {
+    $(document).trigger("commitToGit");
+  });
 
-	$("#commitConfirm").click(function(){
-		var targetBranch = $('#optionNewBranch').is(':checked') ? $("#new-branch-name").val() : repoInfo.branch;
-		var repo = cocodojo.getGithubObj().getRepo(cocodojo.repoOwner, cocodojo.repoName);
-		var message = $("#commitMessage").val();
-		$(document).trigger("commitConfirm");
-		folders = FileFolder.find({codeSessionId: Session.get("codeSessionId"), type: "folder", status: "new"}).get();
-		repo.getRef("heads/" + currentBranch, function(err, latestCommit){
-			if (err) return commitFail(err);
-			if (currentBranch != targetBranch) {
-				repo.createRef({
-					ref: "refs/heads/" + targetBranch, 
-					sha: latestCommit
-				}, function(err, refInfo){
-					if(err) return commitFail(err);
-					var sha = refInfo.object.sha;
-					commitMultipleFiles(repo, folders, docs, sha, targetBranch, message);
-				});	
-			}
-			else {
-				commitMultipleFiles(repo, folders, docs, latestCommit, targetBranch, message);
-			}
-		});
-	});
+  $("#commitConfirm").click(function(){
+    var targetBranch = $('#optionNewBranch').is(':checked') ? $("#new-branch-name").val() : repoInfo.branch;
+    var repo = cocodojo.getGithubObj().getRepo(cocodojo.repoOwner, cocodojo.repoName);
+    var message = $("#commitMessage").val();
+    $(document).trigger("commitConfirm");
+    folders = FileFolder.find({codeSessionId: Session.get("codeSessionId"), type: "folder", status: "new"}).get();
+    repo.getRef("heads/" + currentBranch, function(err, latestCommit){
+      if (err) return commitFail(err);
+      if (currentBranch != targetBranch) {
+        repo.createRef({
+          ref: "refs/heads/" + targetBranch, 
+          sha: latestCommit
+        }, function(err, refInfo){
+          if(err) return commitFail(err);
+          var sha = refInfo.object.sha;
+          commitMultipleFiles(repo, folders, docs, sha, targetBranch, message);
+        }); 
+      }
+      else {
+        commitMultipleFiles(repo, folders, docs, latestCommit, targetBranch, message);
+      }
+    });
+  });
 });
 
 function commitMultipleFiles(repo, folders, docs, refHash, targetBranch, message){
-	var tree = [];
-	var commitIndex = 0;
-	docs.forEach(function(doc){
-		commitFile(doc.path, doc.content, function(err, item){
-			commitIndex += 1;
-			if(err) return commitFail(err);
-			tree.push({
-				"path": item.path,
-				"mode": "100644",
-				"type": "blob",
-				"sha": item.sha
-			});
+  var tree = [];
+  var commitIndex = 0;
+  docs.forEach(function(doc){
+    commitFile(doc.path, doc.content, function(err, item){
+      commitIndex += 1;
+      if(err) return commitFail(err);
+      tree.push({
+        "path": item.path,
+        "mode": "100644",
+        "type": "blob",
+        "sha": item.sha
+      });
 
-			if(commitIndex == docs.length){
-				commit(repo, refHash, tree, targetBranch, message);
-			}
-		});
-	});
+      if(commitIndex == docs.length){
+        commit(repo, refHash, tree, targetBranch, message);
+      }
+    });
+  });
 };
 
 function commitFile(path, fileContent, callback) {
-	cocodojo.getGithubObj().getRepo(cocodojo.repoOwner, cocodojo.repoName).postBlob(fileContent, function(err, sha){
-		callback(err, {sha: sha, path: path});
-	});
+  cocodojo.getGithubObj().getRepo(cocodojo.repoOwner, cocodojo.repoName).postBlob(fileContent, function(err, sha){
+    callback(err, {sha: sha, path: path});
+  });
 };
 
 function commit(repo, refHash, tree, targetBranch, message) {
-	repo.updateTree(refHash, tree, function(err, treeSha){
-		if(err) return commitFail(err);
-		repo.commit(refHash, treeSha, message, function(err, commit) {
-			if(err) return commitFail(err);
+  repo.updateTree(refHash, tree, function(err, treeSha){
+    if(err) return commitFail(err);
+    repo.commit(refHash, treeSha, message, function(err, commit) {
+      if(err) return commitFail(err);
 
-			repo.updateHead(targetBranch, commit, function(err){
-				if(err) {
-					return commitFail(err);
-				}
-				$('#commitBox').modal('hide');
-				$('#commitSuccess').modal('show');
-			});
-		});
-	});
+      repo.updateHead(targetBranch, commit, function(err){
+        if(err) {
+          return commitFail(err);
+        }
+        $('#commitBox').modal('hide');
+        $('#commitSuccess').modal('show');
+      });
+    });
+  });
 }
